dd90155d0a11bce6a91c6a45cf0661176f6e0f08
ecvheo1 <ecvheo@naver.com>
2021-11-04 13:37:56 +0900

    boj14889_ecvheo1.python

---
+++ ecvheo1/Backtracking/14889.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(count, start):
+    global minValue
+    if count == n//2:
+        start = 0
+        link = 0
+        linkTeam = []
+        for i in range(n):
+            if i not in startTeam:
+                linkTeam.append(i)
+
+        for i in range(n//2):
+            for j in range(i+1, n//2):
+                    start += arr[startTeam[i]][startTeam[j]] + arr[startTeam[j]][startTeam[i]]
+                    link += arr[linkTeam[i]][linkTeam[j]] + arr[linkTeam[j]][linkTeam[i]]
+
+        minValue = min(abs(start-link), minValue)
+        return
+    
+    for i in range(start, n):
+        if visited[i]:
+            continue
+        visited[i] = True
+        startTeam[count] = i
+        dfs(count+1, i+1)
+        visited[i] = False
+        
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+startTeam = [0] * (n//2)
+visited = [False] * n
+minValue = 10000000000
+
+dfs(0, 0)
+print(minValue)
