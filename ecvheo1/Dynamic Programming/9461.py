bc4e4cb3e3adcf2043353b69904f0d223f621214
ecvheo1 <ecvheo@naver.com>
2021-11-12 12:26:38 +0900

    boj9461_ecvheo1.python

---
+++ ecvheo1/Dynamic Programming/9461.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+arr = [0] * (101)
+arr[1] = 1
+arr[2] = 1
+arr[3] = 1
+arr[4] = 2
+arr[5] = 2
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    for i in range(6, n+1):
+        arr[i] = arr[i-1] + arr[i-5]
+    print(arr[n])
+
