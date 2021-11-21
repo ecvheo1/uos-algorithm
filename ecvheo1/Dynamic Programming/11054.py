934a16c475b89efdf18e1a5acec7836c08931e1b
ecvheo1 <ecvheo@naver.com>
2021-11-21 11:05:32 +0900

    boj11054_ecvheo1.python

--- ecvheo1/Dynamic Programming/11054.py
+++ ecvheo1/Dynamic Programming/11054.py
@@ -3,3 +3,20 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
+
+d = [ [1 for _ in range(2)] for _ in range(n) ]
+result = 0
+
+for i in range(1, n):
+    for j in range(0, i):
+        if arr[i] > arr[j]:
+            d[i][0] = max(d[i][0], d[j][0]+1)
+
+for i in range(n-1, -1, -1):
+    for j in range(n-1, i, -1):
+        if arr[i] > arr[j]:
+            d[i][1] = max(d[i][1], d[j][1]+1)
+
+for i in range(n):
+    result = max(d[i][0]+d[i][1], result)
+print(result-1)
