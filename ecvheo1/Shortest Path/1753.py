2a41849baaca96e84280f658a1c3831631784d1b
ecvheo1 <ecvheo@naver.com>
2021-11-26 15:15:46 +0900

    boj1753_ecvheo1.python

---
+++ ecvheo1/Shortest Path/1753.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+start = int(input())
+INF = int(1e9)
+graph = [[] for _ in range(1+n)]
+distance = [INF] * (1+n)
+ 
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    distance[start] = 0
+    q = []
+    heapq.heappush(q, (0, start))
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print('INF')
+    else:
+        print(distance[i])
